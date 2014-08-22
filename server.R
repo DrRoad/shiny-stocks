13377a436518762feee94d1b20c063cf94ebc94d
und1f <bash88@gmail.com>
2014-08-21 22:00:08 -0400

    finished app
    Signed-off-by: und1f <bash88@gmail.com>

--- server.R
+++ server.R
@@ -5,14 +5,33 @@ getSymbols(c("GOOG", "AAPL", "MSFT"))
 
 
 shinyServer(function(input, output) {
-	output$stockSymbolOutput <- renderPrint({input$stockSymbol})
+	output$stockSymbolOutput <- renderText({input$stockSymbol})
 	
 	output$stockPlot <- renderPlot({
 		if(input$stockSymbol == "GOOG")
-			candleChart(GOOG, subset='2014') 
+			stockData = GOOG
 		else if(input$stockSymbol == "AAPL")
-			candleChart(AAPL, subset='2014') 
+			stockData = AAPL
 		else if(input$stockSymbol == "MSFT")
-			candleChart(MSFT, subset='2014') 
+			stockData = MSFT
+		
+		
+		if(length(input$indicators) == 0)
+			candleChart(stockData, subset='2014-07::', TA=c(addVo())) 
+		else if("bb" %in% input$indicators && "ma" %in% input$indicators)
+			candleChart(stockData, subset='2014-07::', TA=c(addVo(), addBBands(n = 5), addSMA(col = "white")))
+		else if("bb" %in% input$indicators)
+			candleChart(stockData, subset='2014-07::', TA=c(addVo(), addBBands(n = 5)))
+		else if("ma" %in% input$indicators)
+			candleChart(stockData, subset='2014-07::', TA=c(addVo(), addSMA(col = "white")))
+	})
+	
+	output$lastDividend <- renderText({
+		dividends = getDividends(input$stockSymbol)
+		value = "None"
+		if(length(dividends))
+			value = paste(tail(dividends, 1), "$", sep=" ") 
+		
+		value
 	})
 })
--- ui.R
+++ ui.R
@@ -3,16 +3,33 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(
 	fluidPage(
-		titlePanel("Stock informations"),
-		
+		titlePanel("Stock graph"),
+		br(),
+		helpText("Select a stock symbol from the dropdown box below. This app will automatically display a candlestick chart
+		of that stock price value from July 1st 2014 to now, along with the last dividend for the chosen stock."),
+		helpText("You can also add two technicals indicators on the chart: the Simple Moving Average and the Bollinger Bands. If
+				 you don't know what technical indicators are, feel free to visit the link below to get more insight."),
+		a("Click here to get more information about technical indicators",     
+		  href="http://en.wikipedia.org/wiki/Technical_analysis",
+		  target="_blank"
+		),
+		br(),
+		br(),
+		br(),
 		
 		sidebarLayout(
 			sidebarPanel(
 				selectInput(
 					"stockSymbol", 
-					label = h4("Stock symbol"), 
+					label = strong("Stock symbol"), 
 					choices = list("Google" = "GOOG", "Apple" = "AAPL", "Microsoft" = "MSFT"), 
 					selected = 1
+				),
+				br(),
+				checkboxGroupInput(
+					"indicators", 
+					strong("Technical Indicators :"),
+					c("Moving Average" = "ma", "Bollinger Bands" = "bb")
 				)
 			),
 			
@@ -20,7 +37,11 @@ shinyUI(
 				h4("You've selected :"),
 				verbatimTextOutput("stockSymbolOutput"),
 				br(),
-				plotOutput("stockPlot")
+				plotOutput("stockPlot"),
+				br(),
+				br(),
+				h4("Last dividend :"),
+				verbatimTextOutput("lastDividend")
 			)
 		)
 	)
